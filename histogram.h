5ff945ba7c3f996673ef516ca48dfb4b6a1f2af3
EfimovaJA <100114478+EfimovaJA@users.noreply.github.com>
2022-04-25 14:16:57 +0300

    histogram.h

---
+++ histogram.h
@@ -0,0 +1,9 @@
+#include <vector>
+using namespace std;
+#ifndef HISTOGRAM_H_INCLUDED
+#define HISTOGRAM_H_INCLUDED
+
+void find_minmax(const vector<double> numbers, double& min, double& max);
+vector <size_t> procent (size_t number_count, const vector <size_t> bins);
+
+#endif // HISTOGRAM_H_INCLUDED
